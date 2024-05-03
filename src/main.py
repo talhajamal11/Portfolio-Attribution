1b1c5d7efcb628189724d5c3932d5577b9b1d987
talhajamal11 <talhajamal11@gmail.com>
2024-05-03 12:12:18 +0100

    Added functionality to Pull data for multiple stocks, view data for one stock

--- src/main.py
+++ src/main.py
@@ -1,14 +1,17 @@
 """ Portfolio Attribution Project
 """
 from pull_data import pull_stock_data
+from pull_data import one_stock_data
 
 def main():
     print("------- Downloading Stock Data --------")
-    stocks = ["TSLA"]
+    stocks = ["TSLA", "MSFT"]
     data = pull_stock_data(tickers=stocks,
                            start="2016-01-01",
                            end="2024-01-01")
-    print(data.head())
+    tsla = one_stock_data(dataframe=data,
+                          ticker="TSLA")
+    print(tsla)
     return None
 
 if __name__== "__main__":
--- src/pull_data.py
+++ src/pull_data.py
@@ -16,3 +16,15 @@ def pull_stock_data(tickers:list, start:str, end:str) -> pd.DataFrame:
     """
     df = yf.download(tickers=tickers, start=start, end=end)
     return df
+
+def one_stock_data(dataframe: pd.DataFrame, ticker: str) -> pd.DataFrame:
+    """ Return dataframe with one ticker
+
+    Args:
+        dataframe (pd.DataFrame): Dataframe of Multiple Tickers
+        ticker (str): Stock Ticker
+
+    Returns:
+        pd.DataFrame: Dataframe with Single Ticker
+    """
+    return dataframe.loc[:, (slice(None), ticker)]
