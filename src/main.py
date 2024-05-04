e414d0367ec66f5c9a280c59ba9469e1b346a321
talhajamal11 <talhajamal11@gmail.com>
2024-05-04 09:16:16 +0100

    x

--- src/main.py
+++ src/main.py
@@ -1,12 +1,20 @@
 """ Portfolio Attribution Project
 """
 from portfolio import create_portfolio
+from user_input import get_tickers
+from user_input import get_portfolio_allocation
+from pull_data import pull_stock_data
 
 def main():
     """
     Main Function for Portfolio Attribution Project
     """
-    portfolio = create_portfolio()
+    ticks = get_tickers() # Tickers as a List
+    allocation = get_portfolio_allocation() # Integer Dollar Ammount
+    price_df = pull_stock_data(tickers=ticks,
+                               start="2016-01-01",
+                                end="2024-01-01") # Dataframe of Prices
+    portfolio = create_portfolio(df=price_df, allocation=allocation, ticks=ticks)
     print(portfolio)
     return None
 
--- src/portfolio.py
+++ src/portfolio.py
@@ -2,20 +2,16 @@
 """
 import math
 import pandas as pd
-from user_input import get_tickers
-from user_input import get_portfolio_allocation
-from pull_data import pull_stock_data
 
-def create_portfolio() -> pd.DataFrame:
+
+def create_portfolio(df: pd.DataFrame, allocation: str, ticks: list) -> pd.DataFrame:
     """ Create a Portfolio - Initially this will be an equal weighted portfolio
     Args:
 
     Returns:
         dict: Dataframe of Portfolio
     """
-    ticks = get_tickers() # Tickers as a List
-    allocation = get_portfolio_allocation() # Integer Dollar Ammount
-    df = pull_stock_data(tickers=ticks, start="2016-01-01", end="2024-01-01") # Dataframe of Prices
+
     amount_per_stock = allocation / len(ticks)
     cash = allocation
 
