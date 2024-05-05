e83dfabaea1e885126afb54065cf919c11f0f237
talhajamal11 <talhajamal11@gmail.com>
2024-05-05 10:25:55 +0100

    tested multi-year portfolios successfully

--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ def main():
     # Dataframe of Prices
     price_df = pull_stock_data(tickers=ticks,
                                start="2016-01-04",
-                                end="2016-01-20")
+                                end="2024-01-20")
     # Create Portfolio
     portfolio = create_portfolio(price_df=price_df, allocation=allocation, ticks=ticks)
     print(portfolio)
